e37bf55b967c6f61254273d59791a55ea7f2ac10
Thomas Roughton <t.roughton@me.com>
2016-11-11 16:54:21 +1300

    Added SwiftMath dependency.

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftBGFX",
     dependencies: [
+        .Package(url: "https://github.com/SwiftGFX/SwiftMath", majorVersion: 2),
         .Package(url: "https://github.com/troughton/Cbgfx.git", majorVersion: 1)
     ]
 )
