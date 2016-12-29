0d6f5685f26fe662a1f7fcbd8c5bd629e8f48ce2
troughton <t.roughton@me.com>
2016-12-29 13:39:15 +1300

    Added a nanoseconds method to Timing.

--- Sources/Timing.swift
+++ Sources/Timing.swift
@@ -3,8 +3,8 @@
 //
 
 #if os(OSX) || os(tvOS) || os(iOS)
-	import Darwin
-
+    import Darwin
+    
     public class Timing {
         
         public static var counter: UInt64 {
@@ -14,10 +14,34 @@
         public static let frequency: UInt64 = {
             var info = mach_timebase_info_data_t()
             mach_timebase_info(&info)
-            return 0
+            
+            return UInt64(info.numer) / UInt64(info.denom)
         }()
+        
+        public static var nanoseconds : UInt64 {
+            return self.counter * self.frequency
+        }
     }
 #else
+    
+    import Glibc
+    
+    public class Timing {
+        
+        public static var counter: UInt64 {
+            var tm = timeval()
+            gettimeofday(&tm, nil)
+            return UInt64(tm.tv_sec * 1_000_000 + tm.tv_usec) * 1_000
+        }
+        
+        public static let frequency: UInt64 = {
+            return 1
+        }()
+        
+        public static var nanoseconds : UInt64 {
+            return self.counter
+        }
+    }
 #endif
 
 public struct Counter {
