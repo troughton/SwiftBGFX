5377a11ff9dff11b08cd1f405cdb1aa941e3f482
Thomas Roughton <t.roughton@me.com>
2016-12-31 16:54:37 +1300

    Fixed Cygwin package reference.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,12 @@ import PackageDescription
 let package = Package(
     name: "SwiftBGFX",
     dependencies: [
-        .Package(url: "https://github.com/troughton/SwiftMath", majorVersion: 2),
-        .Package(url: "https://github.com/troughton/Cbgfx.git", majorVersion: 1)
+        .Package(url: "https://github.com/troughton/SwiftMath", majorVersion: 2)
     ]
 )
+
+#if CYGWIN
+package.dependencies.append(Package.Dependency.Package(url: "https://github.com/troughton/Cbgfx-Cygwin.git", majorVersion: 1))
+#else
+package.dependencies.append(Package.Dependency.Package(url: "https://github.com/troughton/Cbgfx.git", majorVersion: 1))
+#endif
